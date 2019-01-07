c2da170a39900cda97261452e39a964ddd82d0ab
Sris Moparthy <smopart@gmail.com>
2019-01-07 14:11:09 -0800

    updated view modal

--- database/config.js
+++ database/config.js
@@ -1,15 +1,15 @@
 //AWS database
-module.exports = {
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'menu_db',
-};
-
-//local database
 // module.exports = {
 //   host: 'localhost',
 //   user: 'root',
-//   password: '',
+//   password: 'password',
 //   database: 'menu_db',
 // };
+
+//local database
+module.exports = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'menu_db',
+};
