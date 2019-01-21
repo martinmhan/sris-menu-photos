7f3101dbbd7801053471cf833b5b73a1d2ac5b58
Martin Han <martinhan@martins-mbp-2.attlocal.net>
2019-01-20 18:42:18 -0800

    Edit bundle filename in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,10 @@
-// var path = require('path');
-// var SRC_DIR = path.join(__dirname, '/client/src');
-// var DIST_DIR = path.join(__dirname, '/client/dist');
-
-// module.exports = {
-//   entry: `${SRC_DIR}/index.jsx`,
-//   output: {
-//     filename: 'bundle.js',
-//     path: DIST_DIR
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.jsx?/,
-//         include: SRC_DIR,
-//         loader: 'babel-loader',
-//         query: {
-//           presets: ['@babel/preset-react', '@babel/preset-env']
-//         }
-//       }
-//     ]
-//   }
-// };
-
-// const path = require('path');
-
-// module.exports = {
-
-//   entry: './client/src/index.jsx',
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.join(__dirname, '/client/dist'),
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: [/\.js$/, /\.jsx?$/],
-//         exclude: /node_modules/,
-//         loader: 'babel-loader',
-//         options: {
-//           presets: ['@babel/preset-env', '@babel/preset-react'],
-//           plugins: ['@babel/plugin-proposal-class-properties'],
-//         },
-//       },
-//       {
-//         test: [/\.css$/],
-//         loaders: ['style-loader', 'css-loader'],
-//         // options: {
-//         //   modules: true,
-//         //   localIdentName: '[hash:base64]'
-//         // },
-//         include: path.join(__dirname, 'client'),
-//       },
-//     ],
-//   },
-// };
-
 const path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: './client/src/index.jsx',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle-menu.js',
     path: path.join(__dirname, '/client/dist'),
   },
   module: {
